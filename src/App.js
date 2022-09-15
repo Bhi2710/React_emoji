47f4bcff93bb0ba8a2f7634a2556738c775ee7ec
Bhi2710 <92436824+Bhi2710@users.noreply.github.com>
2022-09-15 16:51:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
